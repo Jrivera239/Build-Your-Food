68eaa30c0076fd94c0c258ee12850731b1b97a6f
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-09-15 19:06:38 -0400

    homeapge

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
+const session = require("express-session");
+const path = require("path");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+//cookie setup
+const sess = {
+  secret: process.env.DB_COOK,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
